91945e584fe631678f0e374840273d3d58d099fa
Aditya Chowdhry <adityachowdhry09@gmail.com>
2015-10-16 02:08:00 +0530

    InfoWindows added and data updating in infowindows

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,6 +1,14 @@
 class ParkingsController < ApplicationController
   def index
     @parkings=Parking.all
+
+    respond_to do |format|
+
+      format.html # show.html.erb
+      format.json { render json: @parkings }
+
+end
+
   end
 
   def edit
