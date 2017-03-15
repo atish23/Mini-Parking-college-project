8f1816785690fbb44ff82988907177f3a3717de0
Atish <amaske@gmail.com>
2017-03-15 16:56:28 +0530

    cheng

--- app/controllers/api/v1/parkings_controller.rb
+++ app/controllers/api/v1/parkings_controller.rb
@@ -11,7 +11,7 @@ class ParkingsController < ApplicationController
   def show
     @parking=Parking.find(params[:id])
     @parkinglots=@parking.parking_lots
-    respond_to do |format
+    respond_to do |format|
       format.json { render json: @parkinglots }
     end
   end
